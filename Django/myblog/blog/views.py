d69bb10cb7812da8e7247ea0b97d828fca78ca4c
kupets <117300287+kupetss@users.noreply.github.com>
2025-09-19 00:39:47 +0300

    Day 3 Django

--- Django/myblog/blog/views.py
+++ Django/myblog/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def post_list(request):
-    return HttpResponse("Тута будет список постов!")
+    posts = Post.objects.order_by('-created_at')
+    context = {
+        'posts': posts,
+        'title': 'Главная страница'
+    }
+    return render(request, 'blog/post_list.html', context)
